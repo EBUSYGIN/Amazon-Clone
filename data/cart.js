6f0c86f945d28d20bc7a0142860f34c8ec2c6d17
EBUSYGIN <e.busygin2003@gmail.com>
2024-05-24 11:41:48 +0300

    finish static on orders page

--- data/cart.js
+++ data/cart.js
@@ -102,6 +102,13 @@ export function loadCart(fun) {
 }
 
 
+export async function loadCartFetch() {
+  const response = await fetch('https://supersimplebackend.dev/cart')
+  const text = await response.text();
+  console.log(text);
+}
+
+
 
 
 // export function updateDeliveryOption(productId, deliveryOptionId) {
