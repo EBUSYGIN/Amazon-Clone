3ea8935d2d38a98f4b60fe134e85160ea8c3981b
EBUSYGIN <e.busygin2003@gmail.com>
2024-05-25 20:34:20 +0300

    finish delivery indicator

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -26,6 +26,15 @@ async function renderTrackingPage() {
   });
   console.log(orderDetails);
 
+
+  const today = dayjs();
+  const orderTime = dayjs(matchingOrder.orderTime);
+  const deliveryTime = dayjs(orderDetails.estimatedDeliveryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+  
+  
+
+
   let trackingHTML = `
     <div class="order-tracking">
       <a class="back-to-orders-link link-primary" href="orders.html">
@@ -49,19 +58,25 @@ async function renderTrackingPage() {
       <img class="product-image" src="${matchingProduct.image}">
 
       <div class="progress-labels-container">
-        <div class="progress-label">
+        <div class="progress-label ${
+          percentProgress < 50 ? 'current-status' : ''
+        }">
           Preparing
         </div>
-        <div class="progress-label current-status">
+        <div class="progress-label ${
+          (percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''
+        }">
           Shipped
         </div>
-        <div class="progress-label">
+        <div class="progress-label ${
+          percentProgress >= 100 ? "current-status" : ''
+        }">
           Delivered
         </div>
       </div>
 
       <div class="progress-bar-container">
-        <div class="progress-bar"></div>
+        <div class="progress-bar" style="width: ${percentProgress}%;"></div>
       </div>
     </div>
   
