7dca49b02a601dc7e343b1857cae59b2ace0f11b
EBUSYGIN <e.busygin2003@gmail.com>
2024-05-27 13:29:58 +0300

    create search function on main page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -20,13 +20,71 @@ function renderProductsGrid() {
 
   const url = new URL(window.location.href);
   const searchValue = url.searchParams.get('searchValue');
+
   
+  
+  
+  let productsHTML = '';
 
   if (searchValue) {
-     
-  } else {
-    let productsHTML = '';
+    products.forEach((product) => {
+      if (product.name.toLowerCase().includes(searchValue.toLocaleLowerCase())) {
+        productsHTML += `
+          <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
+            </div>
 
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+
+            <div class="product-price">
+              $${formatCurrency(product.priceCents)}
+            </div>
+
+            <div class="product-quantity-container">
+              <select class="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            ${product.extraInfoHTML()}
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart js-added-to-cart-${product.id}">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+              Add to Cart
+            </button>
+          </div>
+          `;
+      }
+     });
+  } else {
+    
     products.forEach((product) => {
       productsHTML += `
       <div class="product-container">
@@ -81,10 +139,9 @@ function renderProductsGrid() {
       </div>
       `;
     });
-    document.querySelector('.js-products-grid').innerHTML = productsHTML;
   }
 
-  
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
 
