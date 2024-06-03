7dd5157605c7212037efa4ad4b13108c323c003f
EBUSYGIN <e.busygin2003@gmail.com>
2024-06-03 16:02:46 +0300

    finish pagination with search

--- scripts/utils/pagination.js
+++ scripts/utils/pagination.js
@@ -1,7 +1,19 @@
-export function renderPagination(products, limit) {
-  for (let i = 1; i < Math.ceil(products.length / limit) + 1; i++) {
+export function renderPagination(count, limit) {
+  for (let i = 1; i < Math.ceil(count / limit) + 1; i++) {
     const li = document.createElement('li')
     document.querySelector('.pagination-list').appendChild(li);
     li.innerHTML = `<button class="js-pagination-button">${i}</button>`;
   }
+}
+
+
+
+export function getPaginationValue(searchValue, products) {
+  let count = 0;
+  products.forEach((product) => {
+    if (product.name.toLowerCase().includes(searchValue.toLowerCase()) || product.keywords.includes(searchValue.toLowerCase())) {
+      count++;
+    }
+  });
+  return count;
 }
